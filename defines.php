b5abf0663685372c2e34b70d06b7c6680a0a7830
AlexHoelli <127683353+AlexHoelli@users.noreply.github.com>
2024-04-04 21:25:20 +0200

    Add files via upload

---
+++ defines.php
@@ -0,0 +1,7 @@
+<?php
+	session_start();
+
+	define( 'FACEBOOK_APP_ID', '1448767872683956' );
+	define( 'FACEBOOK_APP_SECRET', 'efe362b9a9b3e61ae51bb6192632ac2f' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://alexhoelli.github.io/obtaining_access_token.php' );
+	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v5.0/' );
